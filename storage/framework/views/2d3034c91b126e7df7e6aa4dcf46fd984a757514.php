002b6033e03fa3fc14688cebe9e0f907291431be
websturing <websturing.project@gmail.com>
2020-09-05 15:05:09 +0700

    add web portal sijempol

--- storage/framework/views/2d3034c91b126e7df7e6aa4dcf46fd984a757514.php
+++ storage/framework/views/2d3034c91b126e7df7e6aa4dcf46fd984a757514.php
@@ -14,6 +14,12 @@
         href='https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700%7COpen%20Sans:300,400,700%7CMontserrat:700'
         rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="<?php echo e(url('public/caliris/style.css')); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('public/caliris/css/clear.css')); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('public/caliris/css/sm-clean.css')); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('public/caliris/css/slick.css')); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo e(url('public/caliris/css/common.css')); ?>" />
+
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
